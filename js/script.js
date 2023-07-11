933d8375aaf7ddd20a5535310a014fa34e9aaa6e
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-11 06:10:46 +0000

    add css n script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
-});
+$('#tab-contents .tab[id != "tab1"]').hide();
 
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
-  });
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
