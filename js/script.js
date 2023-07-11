b5865444968984cc7a719af5ef575f5ac2e94175
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-11 06:04:25 +0000

    made files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
