f6a674cb26cc3b6dc1fdfc914de7abdf90009adc
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-18 09:33:37 +0000

    add script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,12 @@ $(function() {
     event.preventDefault();
   });
 });
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
